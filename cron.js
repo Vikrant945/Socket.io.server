abf864c34621aae3902d4f31c24719d81a9d3390
Vikrant <vikrant945.k@gmail.com>
2024-08-01 12:33:45 +0530

    ok done added cron to server deleted some logs

--- cron.js
+++ cron.js
@@ -5,7 +5,7 @@ const https = require('https');
 // Replace with your actual backend API endpoint
 const backendUrl = 'https://socket-io-server-vmy8.onrender.com/socket.io/socket.io.js';
 
-const job = new cron.CronJob('*/1 * * * *', function () {
+const job = new cron.CronJob('*/2 * * * *', function () {
   // This function will be executed every 5 minutes.
   // console.log(`Hitting backend endpoint to keep server alive...`);
 
@@ -13,7 +13,7 @@ const job = new cron.CronJob('*/1 * * * *', function () {
   https
     .get(backendUrl, (res) => {
       if (res.statusCode === 200) {
-        console.log('success');
+        // console.log('success');
         // console.log('Successfully hit backend endpoint. Server is active.');
       } else {
         console.error(`Failed to hit endpoint. Status code: ${res.statusCode}`);
